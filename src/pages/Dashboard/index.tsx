ae0960c70af7fd676d2f6369499cd0882976d62c
luccaroli <luccaroli20@gmail.com>
2020-09-17 00:46:47 -0300

    github explorer 1.0 finish

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, FormEvent } from 'react'
+import { Link } from 'react-router-dom'
 
 import { Title, Form, Repositories, Error } from './styles'
 import logoImg from '../../assets/logo.svg'
@@ -75,7 +76,9 @@ const Dashboard: React.FC = () => {
 
       <Repositories>
         {repositories.map(repository => (
-           <a key={repository.full_name} href="teste">
+           <Link 
+           key={repository.full_name} 
+           to={`repositories/${repository.full_name}`}>
             <img 
               src={repository.owner.avatar_url} 
               alt={repository.owner.login} 
@@ -86,7 +89,7 @@ const Dashboard: React.FC = () => {
             </div>
   
             <FiChevronRight size={20} />
-         </a>
+         </Link>
         ))}
       </Repositories>
     </>
